8b33f45fbe2a0051e58668caa25a27aecb6cf20b
prateek251992 <pratikkashiv28@gmail.com>
2024-05-24 13:47:13 +0530

    done

---
+++ projectORS/src/main/java/com/rays/common/message/MessageDAOInt.java
@@ -0,0 +1,11 @@
+package com.rays.common.message;
+
+import com.rays.common.BaseDAOInt;
+
+/**
+ * Role DAO interface.
+ * Prateek Kashiv 
+ */
+public interface MessageDAOInt extends BaseDAOInt<MessageDTO> {
+
+}
